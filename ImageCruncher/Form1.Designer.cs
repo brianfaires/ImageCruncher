74a6811e3e248b3357a270146eddb5a8fbd4d4df
brianfaires <brianfaires@gmail.com>
2017-04-06 11:24:57 -0700

    Add files via upload

---
+++ ImageCruncher/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace ImageCruncher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.btnRecreate = new System.Windows.Forms.Button();
+            this.btnConvertAll = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Location = new System.Drawing.Point(729, 299);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(119, 44);
+            this.btnSelect.TabIndex = 0;
+            this.btnSelect.Text = "Select File";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(237, 299);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(486, 44);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(237, 357);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(486, 44);
+            this.textBox2.TabIndex = 2;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(729, 357);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(119, 44);
+            this.btnSave.TabIndex = 3;
+            this.btnSave.Text = "Save File";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(13, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(622, 281);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(641, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(622, 281);
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // btnRecreate
+            // 
+            this.btnRecreate.Location = new System.Drawing.Point(1049, 299);
+            this.btnRecreate.Name = "btnRecreate";
+            this.btnRecreate.Size = new System.Drawing.Size(214, 52);
+            this.btnRecreate.TabIndex = 6;
+            this.btnRecreate.Text = "Read and recreate";
+            this.btnRecreate.UseVisualStyleBackColor = true;
+            this.btnRecreate.Click += new System.EventHandler(this.btnRecreate_Click);
+            // 
+            // btnConvertAll
+            // 
+            this.btnConvertAll.Location = new System.Drawing.Point(854, 299);
+            this.btnConvertAll.Name = "btnConvertAll";
+            this.btnConvertAll.Size = new System.Drawing.Size(133, 102);
+            this.btnConvertAll.TabIndex = 7;
+            this.btnConvertAll.Text = "Convert Entire Folder";
+            this.btnConvertAll.UseVisualStyleBackColor = true;
+            this.btnConvertAll.Click += new System.EventHandler(this.btnConvertAll_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(24, 302);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(207, 37);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Input File/Dir:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(-2, 360);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(233, 37);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Output File/Dir:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1280, 424);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnConvertAll);
+            this.Controls.Add(this.btnRecreate);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnSelect);
+            this.Name = "Form1";
+            this.Text = "Image Cruncher";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button btnRecreate;
+        private System.Windows.Forms.Button btnConvertAll;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ImageCruncher/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Configuration;
+
+
+namespace ImageCruncher
+{
+    public partial class Form1 : Form
+    {
+        string outputDirPrefix = ConfigurationManager.AppSettings.Get("outputDirPrefix");
+        string fileFormatSuffix = ConfigurationManager.AppSettings.Get("fileExtension");
+        string[] validInputFormats = { ".jpg", ".bmp" };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                openFileDialog1.ShowDialog();
+                textBox1.Text = openFileDialog1.FileName;
+                textBox2.Text = outputDirPrefix + new FileInfo(textBox1.Text).Name.Split('.')[0] + fileFormatSuffix;
+                pictureBox1.Image = Image.FromFile(textBox1.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Exception: " + ex.Message);
+            }
+
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                File.WriteAllBytes(textBox2.Text, CrunchImage(pictureBox1.Image));
+                MessageBox.Show("Successfully saved to:\r\n" + textBox2.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Exception: " + ex.Message);
+            }
+        }
+
+        private void btnRecreate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                byte[] bytes = File.ReadAllBytes(textBox2.Text);
+                pictureBox2.Image = UncrunchImage(bytes);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Exception: " + ex.Message);
+            }
+        }
+
+        private void btnConvertAll_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DirectoryInfo dir = new DirectoryInfo(textBox1.Text);
+                foreach (FileInfo file in dir.GetFiles())
+                {
+                    string newName = outputDirPrefix + file.Name.Split('.')[0] + fileFormatSuffix;
+                    if (validInputFormats.Any(x => file.Name.EndsWith(x)))
+                        File.WriteAllBytes(newName, CrunchImage(Image.FromFile(file.FullName)));
+                }
+
+                MessageBox.Show("Crunched all images in " + dir.Name + " to:\r\n" + outputDirPrefix);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Exception: " + ex.Message);
+            }
+        }
+
+        // Current file format:
+        //  bytes[0:1] = Image height
+        //  bytes[2:3] = Image width
+        //  remaining  = 3 bytes per pixel, column first
+        private byte[] CrunchImage(Image img)
+        {
+            Bitmap bmp = new Bitmap(img);
+            byte[] retVal = new byte[4 + 3 * bmp.Height * bmp.Width];
+            retVal[0] = (byte)((bmp.Height & 0xFF00) >> 8);
+            retVal[1] = (byte)(bmp.Height & 0xFF);
+            retVal[2] = (byte)((bmp.Width & 0xFF00) >> 8);
+            retVal[3] = (byte)(bmp.Width & 0xFF);
+
+            for (int col = 0; col < bmp.Width; col++)
+            {
+                for (int row = 0; row < bmp.Height; row++)
+                {
+                    Color pix = bmp.GetPixel(col, row);
+                    retVal[4 + 3 * (row + col * bmp.Height)] = pix.R;
+                    retVal[4 + 3 * (row + col * bmp.Height) + 1] = pix.G;
+                    retVal[4 + 3 * (row + col * bmp.Height) + 2] = pix.B;
+                }
+            }
+
+            return retVal;
+        }
+        private Bitmap UncrunchImage(byte[] bytes)
+        {
+            int height = (bytes[0] << 8) + bytes[1];
+            int width = (bytes[2] << 8) + bytes[3];
+            Bitmap bmp = new Bitmap(width, height);
+
+            for (int col = 0; col < width; col++)
+            {
+                for (int row = 0; row < height; row++)
+                {
+                    int R = bytes[4 + 3 * (row + col * bmp.Height)];
+                    int G = bytes[4 + 3 * (row + col * bmp.Height) + 1];
+                    int B = bytes[4 + 3 * (row + col * bmp.Height) + 2];
+                    bmp.SetPixel(col, row, Color.FromArgb(R, G, B));
+                }
+            }
+
+            return bmp;
+        }
+    }
+}
